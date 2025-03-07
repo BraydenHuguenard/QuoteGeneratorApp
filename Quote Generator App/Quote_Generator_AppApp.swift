6f2a59025fd4de7e0fe203f9b99eb4649d426ce6
BraydenHuguenard <brayden.huguenard@gmail.com>
2025-03-07 17:30:09 -0500

    Initial Commit

---
+++ Quote Generator App/Quote_Generator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quote_Generator_AppApp.swift
+//  Quote Generator App
+//
+//  Created by Brayden Huguenard on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Quote_Generator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
