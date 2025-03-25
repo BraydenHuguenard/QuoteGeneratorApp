f3761e30e4fd269d89dbf1d8fc63ee238ed28a78
TanviMurkonda <tanvi.murkonda@gmail.com>
2025-03-25 14:03:29 -0400

    Hello

--- Quote Generator App/ViewModels/quoteViewModel.swift
+++ Quote Generator App/ViewModels/quoteViewModel.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
-class QuoteViewModel: ObservableObject {
+struct QuoteGenerator: Identifiable {
+    var id = UUID() // Unique identifier for each quote
+    var quote: String
+    var artist: String
+    var image: String? // Optional image URL for quotes that may not have an image
     
+    // Initializer to create a Quote
+    init(quote: String, artist: String, image: String? = nil) {
+        self.quote = quote
+        self.artist = artist
+        self.image = image
+    }
 }
--- Quote Generator App/Views/homePage.swift
+++ Quote Generator App/Views/homePage.swift
@@ -4,20 +4,79 @@
 //
 //  Created by Brayden Huguenard on 3/7/25.
 //
-
 import SwiftUI
 
 struct HomePage: View {
-    @ObservedObject var quoteVM = QuoteViewModel()
+    @State private var quote = "Click the button to get a quote!"
+    @State private var selectedCategory = "random"
+    
+    let categories = [
+        "Random": "random",
+        "Motivational": "motivational",
+        "Love": "love",
+        "Life": "life"
+    ]
+    
+    func fetchQuote() {
+        guard let categoryEndpoint = categories[selectedCategory] else { return }
+        let url = URL(string: "https://zenquotes.io/api/\(categoryEndpoint)")!
+        
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            if let data = data,
+               let result = try? JSONSerialization.jsonObject(with: data) as? [[String: String]],
+               let firstQuote = result.first?["q"] {
+                DispatchQueue.main.async {
+                    quote = firstQuote
+                }
+            }
+        }.resume()
+    }
     
     var body: some View {
-        VStack {
-            Text("Quote Generator App!!")
+        ZStack {
+            Color.blue.opacity(0.2)
+                .edgesIgnoringSafeArea(.all)
+
+            VStack {
+                
+                Text("Welcome to the Quote Generator App!")
+                    .font(.system(size: 34, weight: .semibold, design: .rounded))
+                    .foregroundColor(.primary)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 50)
+                    .padding(.horizontal, 20)
+                
+                Spacer()
+
+                // Picker to select a category
+                Picker("Select a Quote Category", selection: $selectedCategory) {
+                    ForEach(categories.keys.sorted(), id: \.self) { key in
+                        Text(key).tag(key)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle()) // Make it a segmented control
+                .padding()
+
+                Button("Get Quote") {
+                    fetchQuote()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+
+                Text(quote)
+                    .padding()
+                    .multilineTextAlignment(.center)
+
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    HomePage()
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage()
+    }
 }
